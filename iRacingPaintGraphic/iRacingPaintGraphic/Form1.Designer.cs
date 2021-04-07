cb75f0922fd3e476a6aa6570ff1c266d0e614d67
ddemaegd <ddemaegd82@gmail.com>
2021-04-07 13:49:41 -0400

    Initial Commit

---
+++ iRacingPaintGraphic/iRacingPaintGraphic/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace iRacingPaintGraphic
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RunButton = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.PaintFileTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SaveFileTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CarDropDown = new System.Windows.Forms.ComboBox();
+            this.PaintFileButton = new System.Windows.Forms.Button();
+            this.SaveFileButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(624, 98);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(101, 40);
+            this.RunButton.TabIndex = 0;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMessage.Location = new System.Drawing.Point(624, 169);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 30);
+            this.lblMessage.TabIndex = 1;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // PaintFileTextBox
+            // 
+            this.PaintFileTextBox.Location = new System.Drawing.Point(502, 11);
+            this.PaintFileTextBox.Name = "PaintFileTextBox";
+            this.PaintFileTextBox.Size = new System.Drawing.Size(181, 23);
+            this.PaintFileTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(322, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Paint File Directory:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(322, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Save File Directory:";
+            // 
+            // SaveFileTextBox
+            // 
+            this.SaveFileTextBox.Location = new System.Drawing.Point(502, 49);
+            this.SaveFileTextBox.Name = "SaveFileTextBox";
+            this.SaveFileTextBox.Size = new System.Drawing.Size(181, 23);
+            this.SaveFileTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(19, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Car:";
+            // 
+            // CarDropDown
+            // 
+            this.CarDropDown.FormattingEnabled = true;
+            this.CarDropDown.Location = new System.Drawing.Point(73, 13);
+            this.CarDropDown.Name = "CarDropDown";
+            this.CarDropDown.Size = new System.Drawing.Size(240, 23);
+            this.CarDropDown.TabIndex = 7;
+            // 
+            // PaintFileButton
+            // 
+            this.PaintFileButton.Location = new System.Drawing.Point(689, 11);
+            this.PaintFileButton.Name = "PaintFileButton";
+            this.PaintFileButton.Size = new System.Drawing.Size(36, 23);
+            this.PaintFileButton.TabIndex = 8;
+            this.PaintFileButton.Text = "...";
+            this.PaintFileButton.UseVisualStyleBackColor = true;
+            this.PaintFileButton.Click += new System.EventHandler(this.PaintFileButton_Click);
+            // 
+            // SaveFileButton
+            // 
+            this.SaveFileButton.Location = new System.Drawing.Point(689, 49);
+            this.SaveFileButton.Name = "SaveFileButton";
+            this.SaveFileButton.Size = new System.Drawing.Size(36, 23);
+            this.SaveFileButton.TabIndex = 9;
+            this.SaveFileButton.Text = "...";
+            this.SaveFileButton.UseVisualStyleBackColor = true;
+            this.SaveFileButton.Click += new System.EventHandler(this.SaveFileButton_Click);
+            // 
+            // folderBrowserDialog2
+            // 
+            this.folderBrowserDialog2.HelpRequest += new System.EventHandler(this.folderBrowserDialog2_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 217);
+            this.Controls.Add(this.SaveFileButton);
+            this.Controls.Add(this.PaintFileButton);
+            this.Controls.Add(this.CarDropDown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SaveFileTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PaintFileTextBox);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.RunButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+    #endregion
+
+    private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox PaintFileTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SaveFileTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CarDropDown;
+        private System.Windows.Forms.Button PaintFileButton;
+        private System.Windows.Forms.Button SaveFileButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+    }
+}
+
---
+++ iRacingPaintGraphic/iRacingPaintGraphic/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iRacingPaintGraphic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Dictionary<String, string> carList = new Dictionary<string, string>();
+
+            
+            carList.Add("ARCA Menards Chevrolet Impala", "stockcars2 chevy");
+            carList.Add("Aston Martin DBR9 GT1", "astonmartin dbr9");
+            carList.Add("Audi 90 GTO", "audi90gto");
+            carList.Add("Audi R18", "audir18");
+            carList.Add("Audi R8 LMS", "audir8gt3");
+            carList.Add("Audi RS 3 LMS", "audirs3lms");
+            carList.Add("BMW M4 GT3 - Prototype", "bmwm4gt3");
+            carList.Add("BMW M4 GT4", "bmwm4gt4");
+            carList.Add("BMW M8 GTE", "bmwm8gte");
+            carList.Add("BMW Z4 GT3", "bmwz4gt3");
+            carList.Add("Cadillac CTS-V Racecar", "cadillacctsvr");
+            carList.Add("Chevrolet Corvette C6R GT1", "c6r");
+            carList.Add("Chevrolet Corvette C7 Daytona Prototype", "c7vettedp");
+            carList.Add("Chevrolet Corvette C8.R GTE", "c8rvettegte");
+            carList.Add("Chevrolet Monte Carlo SS", "latemodel");
+            carList.Add("Dallara F3", "dallaraf3");
+            carList.Add("Dallara IR01", "dallarair01");
+            carList.Add("Dallara IR18", "dallarair18");
+            carList.Add("Dallara P217", "dallarap217");
+            carList.Add("Dirt Late Models 350", "dirtlatemodel 350");
+            carList.Add("Dirt Late Models 358", "dirtlatemodel 358");
+            carList.Add("Dirt Late Models 438", "dirtlatemodel 438");
+            carList.Add("Dirt Legends Ford 34 Coupe", "legends dirtford34c");
+            carList.Add("Dirt Midget", "dirtmidget");
+            carList.Add("Dirt Modifieds 358", "dirtmodified 358");
+            carList.Add("Dirt Modifieds BigBlock", "dirtmodified bigblock");
+            carList.Add("Dirt Sprint Cars Winged 305", "dirtsprint winged 305");
+            carList.Add("Dirt Sprint Cars Winged 360", "dirtsprint winged 360");
+            carList.Add("Dirt Sprint Cars Winged 410", "dirtsprint winged 410");
+            carList.Add("Dirt Sprint Cars - Non-Winged 360", "dirtsprint nonwinged 360");
+            carList.Add("Dirt Sprint Cars - Non-Winged 410", "dirtsprint nonwinged 410");
+            carList.Add("Dirt Street Stock", "dirtstreetstock");
+            carList.Add("Dirt UMP Modified", "dirtumpmod");
+            carList.Add("Ferrari 488 GT3", "ferrari488gt3");
+            carList.Add("Ferrari 488 GTE", "ferrari488gte");
+            carList.Add("Ford Fiesta RS WRC", "fordfiestarswrc");
+            carList.Add("Ford GT", "fordgt");
+            carList.Add("Ford GT - 2017", "fordgt2017");
+            carList.Add("Ford GT3", "fordgt gt3");
+            carList.Add("Ford Mustang FR500S", "fr500s");
+            carList.Add("Formula Renault 2.0", "formularenault20");
+            carList.Add("Formula Renault 3.5", "formularenault35");
+            carList.Add("Global Mazda MX-5 Cup", "mx5 mx52016");
+            carList.Add("HPD ARX-01a", "hpdarx01c");
+            carList.Add("Indy Pro 2000 PM-18", "indypropm18");
+            carList.Add("Kia Optima", "kiaoptima");
+            carList.Add("Lamborghini GT3 (placeholder)", "lamborghinievogt3");
+            carList.Add("Legends Ford 34 Coupe", "legends ford34c");
+            carList.Add("Legends Ford 34 Coupe - Rookie", "legends ford34c rookie");
+            carList.Add("Lotus 49", "lotus49");
+            carList.Add("Lotus 79", "lotus79");
+            carList.Add("Lucas Oil Off Road Pro 2 Lite", "protrucks pro2lite");
+            carList.Add("Lucas Oil Off Road Pro 2 Truck", "protrucks pro2truck");
+            carList.Add("Lucas Oil Off Road Pro 4 Truck", "protrucks pro4truck");
+            carList.Add("McLaren 570s GT4", "mclaren570sgt4");
+            carList.Add("McLaren MP4-12C GT3", "mclarenmp4");
+            carList.Add("McLaren MP4-30", "mclarenmp430");
+            carList.Add("Mercedes AMG GT3", "mercedesamggt3");
+            carList.Add("Modifieds", "skmodified");
+            carList.Add("Modifieds Tour", "skmodified tour");
+            carList.Add("NASCAR Chevrolet Monte Carlo - 1987", "stockcars chevymontecarlo87");
+            carList.Add("NASCAR Cup Series Chevrolet Camaro ZL1", "stockcars camarozl12018");
+            carList.Add("NASCAR Cup Series Ford Mustang", "stockcars fordmustang2019");
+            carList.Add("NASCAR Cup Series Toyota Camry", "stockcars toyotacamry");
+            carList.Add("NASCAR Ford Thunderbird - 1987", "stockcars fordthunderbird87");
+            carList.Add("NASCAR Gander Outdoors Chevrolet Silverado", "trucks silverado2019");
+            carList.Add("NASCAR Gander Outdoors Ford F150", "trucks fordf150");
+            carList.Add("NASCAR Gander Outdoors Toyota Tundra", "trucks tundra2015");
+            carList.Add("NASCAR XFINITY Chevrolet Camaro", "stockcars2 camaro2019");
+            carList.Add("NASCAR XFINITY Ford Mustang", "stockcars2 mustang2019");
+            carList.Add("NASCAR XFINITY Toyota Supra", "stockcars2 supra2019");
+            carList.Add("Nissan GTP ZX-T", "nissangtpzxt");
+            carList.Add("Pontiac Solstice", "solstice");
+            carList.Add("Pontiac Solstice - Rookie", "solstice rookie");
+            carList.Add("Porsche 718 Cayman GT4 Clubsport MR", "porsche718gt4");
+            carList.Add("Porsche 911 GT3 Cup (991)", "porsche911cup");
+            carList.Add("Porsche 911 RSR", "porsche991rsr");
+            carList.Add("Porsche 919", "porsche919");
+            carList.Add("Radical SR8", "radical sr8");
+            carList.Add("Ruf RT 12R AWD", "rufrt12r awd");
+            carList.Add("Ruf RT 12R C-Spec", "rufrt12r track cspec");
+            carList.Add("Ruf RT 12R RWD", "rufrt12r rwd");
+            carList.Add("Ruf RT 12R Track", "rufrt12r track");
+            carList.Add("SCCA Spec Racer Ford", "specracer");
+            carList.Add("Silver Crown", "silvercrown");
+            carList.Add("Skip Barber Formula 2000", "rt2000");
+            carList.Add("Sprint Car", "sprint");
+            carList.Add("Street Stock", "streetstock");
+            carList.Add("Subaru WRX STI", "subaruwrxsti");
+            carList.Add("Super Late Model", "superlatemodel");
+            carList.Add("Supercars Ford Mustang GT", "v8supercars fordmustanggt");
+            carList.Add("Supercars Holden ZB Commodore", "v8supercars holden2019");
+            carList.Add("USF 2000", "usf2000usf17");
+            carList.Add("Volkswagen Jetta TDI Cup", "jettatdi");
+            carList.Add("VW Beetle GRC", "vwbeetlegrc");
+            carList.Add("[Legacy] Dallara DW12", "dallaradw12");
+            carList.Add("[Legacy] Indycar Dallara - 2009", "dallara");
+            carList.Add("[Legacy] Mazda MX-5 Cup - 2010", "mx5 cup");
+            carList.Add("[Legacy] Mazda MX-5 Roadster - 2010", "mx5 roadster");
+            carList.Add("[Legacy] NASCAR Cup Chevrolet Impala COT - 2009", "stockcars impala");
+            carList.Add("[Legacy] NASCAR Cup Chevrolet SS - 2013", "stockcars chevyss");
+            carList.Add("[Legacy] NASCAR Cup Ford Fusion - 2016", "stockcars fordfusion");
+            carList.Add("[Legacy] NASCAR Nationwide Chevrolet Impala - 2012", "stockcars2 chevy cot");
+            carList.Add("[Legacy] NASCAR Truck Chevrolet Silverado - 2008", "trucks silverado");
+            carList.Add("[Legacy] NASCAR Xfinity Chevrolet Camaro - 2014", "stockcars2 nwcamaro2014");
+            carList.Add("[Legacy] NASCAR Xfinity Ford Mustang - 2016", "stockcars2 nwford2013");
+            carList.Add("[Legacy] NASCAR Xfinity Toyota Camry - 2015", "stockcars2 camry2015");
+            carList.Add("[Legacy] Pro Mazda", "formulamazda");
+            carList.Add("[Legacy] Riley MkXX Daytona Prototype - 2008", "rileydp");
+            carList.Add("[Legacy] V8 Supercar Ford Falcon - 2009", "fordv8sc");
+            carList.Add("[Legacy] V8 Supercar Ford FG Falcon - 2014", "v8supercars ford2014");
+            carList.Add("[Legacy] V8 Supercar Holden VF Commodore - 2014", "v8supercars holden2014");
+
+
+            CarDropDown.DataSource = new BindingSource(carList, null);
+            CarDropDown.DisplayMember = "Key";
+            CarDropDown.ValueMember = "Value";
+
+            
+
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+
+            lblMessage.Text = "Running...";
+            Random random = new Random();
+
+
+
+
+            string filepath = PaintFileTextBox.Text + @"\" +  CarDropDown.SelectedValue;
+            DirectoryInfo d = new DirectoryInfo(filepath);
+
+            string saveLocation = SaveFileTextBox.Text + @"\" + CarDropDown.SelectedItem + @"\" + random.Next(1, 1000) + @"\";
+
+            foreach (var file in d.GetFiles("*.tga"))
+            {
+
+                string imageUrl = @"http://localhost:32034/pk_car.png?size=2&view=1&carPath=" + CarDropDown.SelectedValue + "&number=82&carCol=FFFFFF,000000,000000&carCustPaint=" + PaintFileTextBox.Text +  @"\" + CarDropDown.SelectedValue + @"\" + file.Name;
+
+                byte[] imageBytes;
+                HttpWebRequest imageRequest = (HttpWebRequest)WebRequest.Create(imageUrl);
+                WebResponse imageResponse = imageRequest.GetResponse();
+
+                Stream responseStream = imageResponse.GetResponseStream();
+
+                using (BinaryReader br = new BinaryReader(responseStream))
+                {
+                    imageBytes = br.ReadBytes(500000);
+                    br.Close();
+                }
+                responseStream.Close();
+                imageResponse.Close();
+                
+                
+                System.IO.Directory.CreateDirectory(saveLocation);
+
+                FileStream fs = new FileStream(saveLocation + file.Name.Replace(file.Extension,"") + ".png", FileMode.Create);
+                BinaryWriter bw = new BinaryWriter(fs);
+                try
+                {
+                    bw.Write(imageBytes);
+                }
+                finally
+                {
+                    fs.Close();
+                    bw.Close();
+                }
+            }
+
+            lblMessage.Text = "Done!";
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void PaintFileButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = this.folderBrowserDialog1;
+            DialogResult result = folderDlg.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                PaintFileTextBox.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+        private void folderBrowserDialog2_HelpRequest(object sender, EventArgs e)
+        {
+        }
+
+        private void SaveFileButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = this.folderBrowserDialog2;
+            DialogResult result = folderDlg.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                SaveFileTextBox.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+    }
+}
