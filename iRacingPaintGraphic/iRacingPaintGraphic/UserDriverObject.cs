dc3737307de500383d59d81553ebfb0d2a224aec
ddemaegd <ddemaegd82@gmail.com>
2022-04-05 20:08:31 -0400

    Added ability to merge files into one image

---
+++ iRacingPaintGraphic/iRacingPaintGraphic/UserDriverObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iRacingPaintGraphic
+{
+    class UserDriverObject
+    {
+        public string ID;
+        public string Name;
+        public string Number;
+        public string RimColor;
+        public string NumberColor1;
+        public string NumberColor2;
+        public string NumberColor3;
+        public string File;
+
+    }
+}
